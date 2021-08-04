8dda3bc172d9cfadda995fcb6c593861c317d6b1
Tasha Davis <tdavis0831@gmail.com>
2021-08-04 23:34:27 +0000

    bot exists

--- markov.py
+++ markov.py
@@ -1,7 +1,8 @@
 """A Markov chain generator that can tweet random messages."""
-
+import os
 import sys
 from random import choice
+import discord
 
 
 def open_and_read_file(filenames):
@@ -64,3 +65,20 @@ text = open_and_read_file(filenames)
 
 # Get a Markov chain
 chains = make_chains(text)
+
+
+
+client = discord.Client()
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(os.environ['DISCORD_TOKEN'])
